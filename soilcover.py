72b7fcd057cbe2ff5b6463f763b438a87d54c7f1
Laenor Brandão <laenor.henrique@estudante.ifgoiano.edu.br>
2026-06-18 20:56:01 -0300

    Update soilcover.py

--- soilcover.py
+++ soilcover.py
@@ -1,9 +1,6 @@
-"""
-Módulos do SoilCarbon Planner
-"""
-"""
-Funções utilitárias compartilhadas entre os módulos
-"""
+"Módulos do SoilCarbon Planner"
+
+"""Funções utilitárias compartilhadas entre os módulos"""
 import streamlit as st
 import pandas as pd
 import numpy as np
